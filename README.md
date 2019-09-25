e4f4d0383f588b7cf5214ba826cfd56004ee5af3
chaojiang <zhouchaojiang@bigdata-ai.tech>
2019-09-25 10:33:20 +0800

    提交

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # yahou-cli
 构建自己的脚手架
+
+`
+	更多学习参考 https://github.com/vuejs/vue-cli
--- src/utils.js
+++ src/utils.js
@@ -12,7 +12,7 @@ function getPackageVersion() {
 }
 
 function logPackageVersion() {
-  const msg = `zero-cli version: ${getPackageVersion()}`;
+  const msg = `yahou-cli version: ${getPackageVersion()}`;
   console.log();
   console.log(msg);
   console.log();
